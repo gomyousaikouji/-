b3d01cc449f896197f0fb20b15ef3a4361dbf59d
gomyousaikouji <gomyou.saikouji@gmail.com>
2026-03-02 17:19:42 +0900

    Update ichou.js

--- ichou.js
+++ ichou.js
@@ -1,1 +1,29 @@
 
+const area =
+document.querySelector(".ichou-area");
+
+function createLeaf(){
+
+const leaf =
+document.createElement("img");
+
+leaf.src="images/ichou.png";
+leaf.classList.add("ichou");
+
+leaf.style.left=
+Math.random()*100+"vw";
+
+leaf.style.width=
+20+Math.random()*30+"px";
+
+leaf.style.animationDuration=
+6+Math.random()*6+"s";
+
+area.appendChild(leaf);
+
+setTimeout(()=>{
+leaf.remove();
+},12000);
+}
+
+setInterval(createLeaf,600);
